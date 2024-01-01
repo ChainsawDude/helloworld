ef1fa11b5bf6f90c4a5528903a6bd6356e088d86
Cainsaw <g9344021@gmail.com>
2024-01-01 12:17:33 +0100

    add vscode settings

---
+++ hello.py
@@ -0,0 +1,4 @@
+
+def hello() -> int:
+    print("Hello World")
+    return 5
---
+++ test/test_hello.py
@@ -0,0 +1,6 @@
+"""This is a test of the hell() function"""
+
+from hello import hello
+
+def test_hello():
+    assert hello() == 5

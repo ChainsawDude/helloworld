222da4bf258a86d4c8fc39c85f91f63a8d62df88
Cainsaw <g9344021@gmail.com>
2024-01-01 13:15:18 +0100

    add return_hunter and tests

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 def hello() -> int:
     print("Hello World")
     return 5
+
+
+def return_hunter() -> str:
+    return "Hunter"
--- test/test_hello.py
+++ test/test_hello.py
@@ -1,7 +1,11 @@
 """This is a test of the hell() function"""
 
 from hello import hello
+from hello import return_hunter
 
 
 def test_hello():
     assert hello() == 5
+
+def test_return_hunter():
+    assert return_hunter() == "Hunter"
